42699b4e13f252fa5c500879daa312d17703e356
Iain Tooley <iaintooley1@gmail.com>
2020-02-28 01:16:47 +0000

    not working

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import SimpleMap from "../components/SimpleMap"
 
 class Main extends Component {
   constructor(props){
@@ -8,9 +9,7 @@ class Main extends Component {
 
   render(){
     return(
-      <div>
-      <p>Hi</p>
-      </div>
+      <SimpleMap />
     )
   }
 }
